c9ee02a032633de0d40f433c1d6421f20f72e629
T Lee <tleehoc@gmail.com>
2012-06-01 20:30:36 -0700

    Add comments

--- getNetBIOS.sh
+++ getNetBIOS.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo -n "Starting "
+#
+#  Gets the NetBIOS names of a bunch of local network devices
+#
+#  Author: Thomas Lee
+#  Date:   Jun 01, 2012
+
+# Change to script directory
+cd "$( cd "$( dirname "$0" )" && pwd )"
+sudo echo -n "Starting "
 nmap -version | tr -d '\n'
 echo -n " at "
 date
