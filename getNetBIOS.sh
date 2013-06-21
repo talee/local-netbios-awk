abff4e0d4edab98ff688095a1b6c665fb5c94976
Thomas Lee <tleehoc@gmail.com>
2013-06-21 00:06:14 -0700

    Change IP range

--- getNetBIOS.sh
+++ getNetBIOS.sh
@@ -11,4 +11,4 @@ sudo echo -n "Starting "
 nmap -version | tr -d '\n'
 echo -n " at "
 date
-sudo nmap -sU --script nbstat.nse -p137 10.0.0.1-15 | awk -f formatNetBIOS.awk | grep --color ' [^ ]\{3,\} $\|$'
+sudo nmap -sU --script nbstat.nse -p137 192.168.42.1-150 | awk -f formatNetBIOS.awk | grep --color ' [^ ]\{3,\} $\|$'
